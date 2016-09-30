2c4a402b7f2e17228c75d92fce8de3ecb12ace06
Peter Pajer <peter.p.pajer@gmail.com>
2016-09-30 16:15:46 +0200

    move from using IMDB+oMDB to TMDB as media source

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -8,6 +8,9 @@ const oMDB 			= require('omdb');
 const IMDB 			= require('imdb-api');
 const MovieDB 		= require('moviedb')('bca1b28150defdd6e20032c1cfcb36ae');
 
+const smallImageBaseUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
+const largeImageBaseUrl = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2';
+
 var $searchForm 		= $('#main-search-form'),
 	$searchSubmit 		= $('#main-search-submit'),
 	$search 			= $('#main-search'),
@@ -16,6 +19,8 @@ var $searchForm 		= $('#main-search-form'),
 	$homeSection		= $('#home'),
 	$moviesSection 		= $('#movies'),
 	$movieInfoSection 	= $('#movie-info'),
+	$seriesInfoSection 	= $('#series-info'),
+	$seasonInfoSection 	= $('#season-info'),
 	$searchTorrents 	= $('#movie-search-torrents'),
 	$torrentSection 	= $('#torrents'),
 	$playerSection 		= $('#player'),
@@ -23,8 +28,7 @@ var $searchForm 		= $('#main-search-form'),
 	$torrentsList 		= $('#torrents-list'),
 	$moviesList 		= $('#movies-list'),
 	$loadingSection 	= $('#loading'),
-	$loadingInfo		= $('#loading-info'),
-	movieToSearch;
+	$loadingInfo		= $('#loading-info');
 
 // Ugly code for a beautiful init
 setTimeout(function() {
@@ -36,27 +40,18 @@ setTimeout(function() {
 $searchForm.on('submit', function(e) {
 	e.preventDefault();
 
-	var $toClone 	= $('#movies .movie');
-	
-	//Global
-	movieToSearch = $search.val()
+	var $toClone 	= $('#movies .movie'),
+		movieToSearch = $search.val();
 	
 	setLoadingState(true);
 
-	oMDB.search(movieToSearch, function(err, response) {
-		if (err) {console.log(err);}
-
-		if (response.length > 1) {
-			buildMoviesList(response, $toClone.clone());	
-		} else {
-			IMDB.getReq({id: response[0].imdb}, function(err, res) {
-				if (err) console.log(err);
-
-				displayMovieInfo(false, res);
-			})
-		}
-	});
-
+	MovieDB.searchMulti({query:movieToSearch}, function(err, res) {
+		if (err) {
+			console.log(err);
+		} else{
+			buildMediaList(res.results, $toClone.first().clone());
+		};
+	})
 });
 
 $searchTorrents.on('click', function(e) {
@@ -66,26 +61,16 @@ $searchTorrents.on('click', function(e) {
 	var $toClone = $('#torrents .torrent'),
 		toSearch = $searchTorrents.attr('data-movietitle');
 
-	ThePirateBay.search(toSearch, {
-		category: 'video',
-		page: 0,
-		orderBy: 'seeds',
-		order: 'desc'
-	}).then(function(results) {
-		buildResultsList(results, $toClone.clone());
+	searchTorrents(toSearch).then(function(results) {
+		buildResultsList(results, $toClone.first().clone());
 	}).catch(function(err){
 		console.log('first try:',err);
 
 		$loadingInfo.html('There seems to be a problem with The Pirate Bay, retrying...');
 		setTimeout(function(){
 
-			ThePirateBay.search(toSearch, {
-				category: 'video',
-				page: 0,
-				orderBy: 'seeds',
-				order: 'desc'
-			}).then(function(results) {
-				buildResultsList(results, $toClone.clone());
+			searchTorrents(toSearch).then(function(results) {
+				buildResultsList(results, $toClone.first().clone());
 			}).catch(function(err){
 				console.log('retry:', err);
 
@@ -126,47 +111,77 @@ function cloneAndFill($element, data, className) {
 	return $clone;
 }
 
+function fillCard($cloneElement, name, year, id, poster, selector, onclick) {
+	if (parseInt(year) > new Date().getFullYear()) return;
+	var cloneData = {
+		'title': name,
+		'year': year
+	},
+	$clone = cloneAndFill($cloneElement, cloneData, selector);
+	$clone.find(selector+'-poster').attr('src', smallImageBaseUrl+poster);
+	$clone.attr('data-id', id);
+	$clone.on('click', function(e) {
+		onclick($(this).attr('data-id'));
+	});
+	console.log($clone);
+	return $clone;
+}
+
+function getMediaGenres(genres) {
+	var genreList = [];
+	genres.forEach(function(genre) {
+		genreList.push(genre.name);
+	});
+	return genreList;
+}
+
+function getMediaCredits(credits) {
+	var creditsList = {
+		'director': [],
+		'writer': [],
+		'cast': []
+	};
+	credits.crew.forEach(function(c){
+		if (c.job === 'Director') creditsList.director.push(c.name);
+		if (c.job === 'Writer') creditsList.writer.push(c.name);
+	});
+	for (var i = 0; i < 5; i++) {
+		if (credits.cast[i].name) creditsList.cast.push(credits.cast[i].name);
+	};
+
+	return creditsList;
+}
+
 /*
 
 	MOVIE SELECT
 
 */
 
-function buildMoviesList(movies, $toClone) {
+function buildMediaList(media, $toClone) {
 
 	$moviesList.empty();
 
-	for (var i = 0; i < movies.length; i++) {
+	for (var i = 0; i < media.length; i++) {
 
-		if (movies[i].imdb) {
-
-			IMDB.getReq({id: movies[i].imdb}, function(err, res) {
-				if (err) {console.log(err);}
-
-				$moviesList.append(fillMovieCard(res, $toClone));
-			})
-		}
+		if (media[i].media_type === 'movie') {
+			$moviesList.append(fillMovieCard(media[i], $toClone));
+		} else if (media[i].media_type === "tv") {
+			$moviesList.append(fillSeriesCard(media[i], $toClone));
+		} 
 	}
 
 	setLoadingState(false, $moviesSection);
 }
 
 function fillMovieCard(movie, $cloneElement) {
-	if ((!movie.poster || movie.poster.indexOf('http') === -1) || !movie.title || !movie._year_data || !movie.plot) {
-		return;
-	}
-	var cloneData = {
-		'title' : movie.title,
-		'year' : movie._year_data
-	},
-	$clone = cloneAndFill($cloneElement, cloneData, '.movie');
-	$clone.find('.movie-poster').attr('src', movie.poster);
-	$clone.attr('data-imdbid', movie.imdbid);
-	$clone.on('click', function(e) {
-		displayMovieInfo($(this).attr('data-imdbid'));
-	});
+	if (!movie.poster_path || (movie.poster_path === null)) return;
+	return fillCard($cloneElement, movie.title, movie.release_date.split('-')[0], movie.id, movie.poster_path, '.movie', displayMovieInfo);
+}
 
-	return $clone;
+function fillSeriesCard(series, $cloneElement) {
+	if (!series.poster_path || (series.poster_path === null)) return;
+	return fillCard($cloneElement, series.name, series.first_air_date.split('-')[0], series.id, series.poster_path, '.movie', displaySeriesInfo);
 }
 
 /*
@@ -175,51 +190,41 @@ function fillMovieCard(movie, $cloneElement) {
 
 */
 
-function displayMovieInfo(imdbid, movieData) {
-	var $movieInfo 	= $('#movie-info');
+function displayMovieInfo(id) {
 
 	setLoadingState(true);
+	MovieDB.movieInfo({id: id}, function(err, movie) {
+		if (err) {
+			console.log(err);
+		} else {
 
-	// We already have the data
-	if ((!imdbid) && movieData) {
-
-		fillMovieInfo($movieInfo, movieData);
-		setLoadingState(false, $movieInfoSection);
-
-	// We need to query the data
-	} else {
-		
-		IMDB.getReq({id: imdbid}, function(err, movie) {
-			if (err) {
-				console.log('error: ', err);
-				oMDB.get({imdb: imdbid}, {fullPlot: true}, function(err, res) {
-					if (err) console.log(err);
-					console.log('res:', res);
-					fillMovieInfo($movieInfo, res[0]);
+			MovieDB.movieCredits({id: movie.id}, function(err, credits) {
+				if (err) {
+					console.log(err);
+				} else {
+					fillMovieInfo(movie, credits);
 					setLoadingState(false, $movieInfoSection);
-				});
-			} else {
-				console.log('res:' ,movie)
-				fillMovieInfo($movieInfo, movie);
-				setLoadingState(false, $movieInfoSection);
-			}
-		});
-	}
+				}
+			})
+		}
+	});
 }
 
-function fillMovieInfo($movieInfo, movieData) {
-	console.log(movieData);
-	$movieInfo.find('.movie-title').html(movieData.title+'<small class="movie-year"></small>'); // hanging by a thread here
-	$movieInfo.find('.movie-year').html(movieData._year_data || movieData.year);
-	$movieInfo.find('.movie-genre').html(movieData.genres);
-	$movieInfo.find('.movie-director').html(movieData.director);
-	$movieInfo.find('.movie-writer').html(movieData.writer);
-	$movieInfo.find('.movie-actors').html(movieData.actors);
-	$movieInfo.find('.movie-plot').html(movieData.plot);
-	$movieInfo.find('.movie-runtime').html(movieData.runtime);
-	$movieInfo.find('.movie-rating').html(movieData.rating || movieData.imdb.rating);
-	$movieInfo.find('.movie-poster').attr('src', movieData.poster);
-	$searchTorrents.attr('data-movietitle', movieData.title);
+function fillMovieInfo(movieData, creditsData) {
+	var genres 	= getMediaGenres(movieData.genres),
+		credits = getMediaCredits(creditsData);
+
+	$movieInfoSection.find('.movie-title').html(movieData.title+'<small class="movie-year"></small>'); // hanging by a thread here
+	$movieInfoSection.find('.movie-year').html(movieData.release_date.split('-')[0]);
+	$movieInfoSection.find('.movie-genre').html(genres.join(', '));
+	$movieInfoSection.find('.movie-director').html(credits.director.join(', '));
+	$movieInfoSection.find('.movie-writer').html(credits.writer.join(', '));
+	$movieInfoSection.find('.movie-actors').html(credits.cast.join(', '));
+	$movieInfoSection.find('.movie-plot').html(movieData.overview);
+	$movieInfoSection.find('.movie-runtime').html(movieData.runtime);
+	$movieInfoSection.find('.movie-rating').html(movieData.vote_average);
+	$movieInfoSection.find('.movie-poster').attr('src', largeImageBaseUrl+movieData.poster_path);
+	$searchTorrents.attr('data-movietitle', movieData.title+' '+movieData.release_date.split('-')[0]); // Identify by title+year for easy torrent search
 }
 
 /*
@@ -228,21 +233,144 @@ function fillMovieInfo($movieInfo, movieData) {
 
 */
 
-function displaySeriesInfo($seriesInfo, seriesData) {
+function displaySeriesInfo(id) {
 
+	setLoadingState(true);
+	MovieDB.tvInfo({id: id}, function(err, series) {
+		if (err) {
+			console.log(err);
+		} else {
+			MovieDB.tvCredits({id: id}, function(err, credits) {
+				if (err) {
+					console.log(err);
+				} else {
+					fillSeriesInfo(series, credits);
+					setLoadingState(false, $seriesInfoSection);
+				};
+			})
+		};
+	})
 }
 
-function fillSeriesInfo($seriesInfo, seriesData) {
-
+function fillSeriesInfo(seriesData, creditsData) {
+	var genres 		= getMediaGenres(seriesData.genres),
+		credits 	= getMediaCredits(creditsData),
+		$toClone 	= $seriesInfoSection.find('.series-season').clone();
+	$seriesInfoSection.find('#series-seasons').empty();
+	$seriesInfoSection.find('.series-title').html(seriesData.name+'<small class="series-year"></small>'); // hanging by a thread here
+	$seriesInfoSection.find('.series-year').html(seriesData.first_air_date.split('-')[0]);
+	$seriesInfoSection.find('.series-genre').html(genres.join(', '));
+	$seriesInfoSection.find('.series-director').html(credits.director.join(', '));
+	$seriesInfoSection.find('.series-writer').html(credits.writer.join(', '));
+	$seriesInfoSection.find('.series-actors').html(credits.cast.join(', '));
+	$seriesInfoSection.find('.series-plot').html(seriesData.overview);
+	$seriesInfoSection.find('.series-rating').html(seriesData.vote_average);
+	$seriesInfoSection.find('.series-poster').attr('src', largeImageBaseUrl+seriesData.poster_path);
+	seriesData.seasons.forEach(function(season) {
+		$seriesInfoSection.find('#series-seasons').append(fillSeasonCard(seriesData.name, seriesData.id, season, $toClone));
+	})
 }
 
-function displaySeasonInfo($seasonInfo, seasonData) {}
+function fillSeasonCard(series, seriesId, season, $cloneElement) {
+	var cloneData = {
+			'title': 'Season '+season.season_number,
+			'year': season.air_date ? season.air_date.split('-')[0] : '',
+		},
+		$clone = cloneAndFill($cloneElement.clone(), cloneData, '.season');
+	$clone.find('.season-poster').attr('src', smallImageBaseUrl+season.poster_path);
+	$clone.attr('data-seriesid', seriesId);
+	$clone.attr('data-seasonnumber', season.season_number);
+	$clone.attr('data-series', series);
+	$clone.on('click', function(e) {
+		displaySeasonInfo($(this).attr('data-series'), $(this).attr('data-seriesid'), $(this).attr('data-seasonnumber'));
+	});
+	return $clone;
+}
+
+/*
+
+	SEASON INFO
+
+*/
+
+function displaySeasonInfo(seriesName, id, number) {
+	setLoadingState(true);
+
+	MovieDB.tvSeasonInfo({id:id, season_number: number}, function(err, season) {
+		if (err) {
+			console.log(err);
+		} else {
+			fillSeasonInfo(season, seriesName);
+			setLoadingState(false, $seasonInfoSection);
+		};
+	})
+}
+
+function fillSeasonInfo(seasonData, seriesName) {
+	$seasonInfoSection.find('.season-title').html('Season '+seasonData.season_number+': '+seasonData.name+'<small class="season-year"></small>');
+	$seasonInfoSection.find('.season-year').html(seasonData.air_date ? seasonData.air_date.split('-')[0] : '');
+	$seasonInfoSection.find('.season-poster').attr('src', largeImageBaseUrl+seasonData.poster_path);
+	seasonData.episodes.forEach(function(episode) {
+		$seasonInfoSection.find('#season-episodes').append(fillEpisodeCard(episode, seasonData.season_number, seriesName));
+	});
+}
+
+function fillEpisodeCard(episodeData, seasonNumber, seriesName) {
+	var $toClone = $seasonInfoSection.find('.season-episode').clone(),
+		cloneData = {
+			'number': episodeData.episode_number,
+			'name': episodeData.name,
+			'overview': episodeData.overview
+		},
+		$clone = cloneAndFill($toClone, cloneData, '.episode');
+
+	$clone.find('.episode-play').attr('data-search', seriesName+' S'+seasonNumber+'E'+episodeData.episode_number);
+	$clone.on('click', function(e) {
+		setLoadingState(true);
+
+		var $toClone = $('#torrents .torrent');
+
+		searchTorrents($(this).attr('data-search')).then(function(results) {
+			buildResultsList(results);
+		}).catch(function(err){
+			console.log('first try:',err);
+
+			$loadingInfo.html('There seems to be a problem with The Pirate Bay, retrying...');
+			setTimeout(function(){
+
+				searchTorrents(toSearch).then(function(results) {
+					buildResultsList(results, $toClone.clone());
+				}).catch(function(err){
+					console.log('retry:', err);
+
+					$loadingInfo.html('The server seems to be down, try again in a few!<br>Cancelling search...');
+					setTimeout(function(){
+						setLoadingState(false, $homeSection);
+						$loadingInfo.html('This may take a few seconds');
+					}, 2000);
+				});
+			}, 3000);
+		});
+	});
+
+	return $clone;
+}
 
 /*
 
 	TORRENT SELECT
 
 */
+
+function searchTorrents(toSearch) {
+
+	return ThePirateBay.search(toSearch, {
+		category: 'video',
+		page: 0,
+		orderBy: 'seeds',
+		order: 'desc'
+	})
+}
 
 function buildResultsList(results, $toClone) {
 
